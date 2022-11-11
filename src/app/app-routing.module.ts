f1ba778351b3ffdc0a561949fb6945d0872eaa90
OscarJaVaz <oscarjandetevazquez@gmail.com>
2022-11-11 11:29:03 -0600

    version 1.4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ import {DonativosComponent} from "./view/donativos/donativos.component";
 import { ContactoComponent } from './view/contacto/contacto.component';
 import { IsesionComponent } from './view/isesion/isesion.component';
 import { RegistroComponent } from './view/registro/registro.component';
+import { RecuperarComponent } from './view/recuperar/recuperar.component';
+import { MenuComponent } from './cabecera/menu/menu.component';
+import { ActividadesComponent } from './view/actividades/actividades.component';
+import { CampanaComponent } from './view/campana/campana.component';
+import { UsuarioperfilComponent } from './view/usuarioperfil/usuarioperfil.component';
+import { EditarusuComponent } from './view/editarusu/editarusu.component';
+import { ActivityComponent } from './view/activity/activity.component';
+import { RegistroactComponent } from './view/registroact/registroact.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -13,7 +21,14 @@ const routes: Routes = [
   {path: 'isesion', component: IsesionComponent, data:{title: 'LotoTres A.C Sesion'}},
   {path: 'contacto', component: ContactoComponent, data:{title: 'LotoTres A.C Contacto'}},
   {path: 'registro', component: RegistroComponent, data:{title: 'LotoTres A.C Registro'}},
-
+  {path: 'recuperar', component: RecuperarComponent, data:{title: 'LotoTres A.C Recuperar'}},
+  {path: 'menu', component: MenuComponent, data:{title: 'LotoTres Menu'}},
+  {path: 'actividades', component: ActividadesComponent, data:{title: 'LotoTres Actividades'}},
+  {path: 'campaña', component: CampanaComponent, data:{title: 'lOTOtRES Campaña'} },
+  {path: 'perfil', component: UsuarioperfilComponent, data:{title: 'LotoTres Perfil'}},
+  {path: 'editar', component: EditarusuComponent, data:{title:'LotoTres Editar'}},
+  {path: 'actividad', component: ActivityComponent, data:{title: 'LotoTres Activity'}},
+  {path: 'registrarse', component: RegistroactComponent, data:{title: 'LotoTres Registroact'}},
 ];
 
 @NgModule({
@@ -21,4 +36,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponents = [HomeComponent, DonativosComponent, IsesionComponent, ContactoComponent, RegistroComponent];
+export const RoutingComponents = [HomeComponent, DonativosComponent, IsesionComponent, ContactoComponent, RegistroComponent, RecuperarComponent,
+   MenuComponent, ActividadesComponent, CampanaComponent, UsuarioperfilComponent, EditarusuComponent,
+  ActivityComponent, RegistroactComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,16 @@ import { IsesionComponent } from './view/isesion/isesion.component';
 import { ContactoComponent } from './view/contacto/contacto.component';
 import { RegistroComponent } from './view/registro/registro.component';
 import { FooterComponent } from './footer/footer/footer.component';
+import { SliderComponent } from './app/slider/slider.component';
+import { RecuperarComponent } from './view/recuperar/recuperar.component';
+import { ActividadesComponent } from './view/actividades/actividades.component';
+import { CampanaComponent } from './view/campana/campana.component';
+import { UsuarioperfilComponent } from './view/usuarioperfil/usuarioperfil.component';
+import { EditarusuComponent } from './view/editarusu/editarusu.component';
+import { ActivityComponent } from './view/activity/activity.component';
+import { RegistroactComponent } from './view/registroact/registroact.component';
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +31,16 @@ import { FooterComponent } from './footer/footer/footer.component';
     IsesionComponent,
     ContactoComponent,
     RegistroComponent,
-    FooterComponent
+    FooterComponent,
+    SliderComponent,
+    RecuperarComponent,
+    ActividadesComponent,
+    CampanaComponent,
+    UsuarioperfilComponent,
+    EditarusuComponent,
+    ActivityComponent,
+    RegistroactComponent,
+   
   ],
   imports: [
     BrowserModule,
