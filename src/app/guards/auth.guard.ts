bb58832f6a79ce095f7e05e05aec144e3f007bdf
Jedi1906 <jedidiah.hernandez@xiaexperience.com>
2022-11-22 19:11:56 -0600

    ajustes main.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.authService.isAuthenticated()) {
-      this.router.navigate(['/login']).then(() => {});
+      this.router.navigate(['/home']).then(() => {});
       return false;
     }
     return true;
