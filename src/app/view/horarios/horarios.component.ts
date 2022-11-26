f3dd571ecb83b473fc763fb338759f9cb68381ec
af205404 <af205404@gmail.com>
2022-11-25 20:18:22 -0600

    Avance

--- src/app/view/horarios/horarios.component.ts
+++ src/app/view/horarios/horarios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Actividad } from 'src/app/models/actividad';
+import { GactividadComponent } from '../gactividad/gactividad.component';
+import { IhorariosComponent } from '../ihorarios/ihorarios.component';
 
 @Component({
   selector: 'app-horarios',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorariosComponent implements OnInit {
 
+dias: string []=["Lunes","Martes","Miercoles","Jueves", "viernes"]
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
